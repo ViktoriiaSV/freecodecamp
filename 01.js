4aea27226f2c3023dd186d386b42f8c40a79fbad
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2022-02-17 02:40:28 +0200

    first day

---
+++ 01.js
@@ -0,0 +1,52 @@
+//Basic JavaScript#
+
+//1
+//first
+/* second */
+
+//2
+var myName;
+
+//3
+var a;
+a = 7;
+
+//4
+var a;
+a = 7;
+var b;
+b = a;
+
+//5
+var a  = 9;
+
+//6
+var myFirstName = 'Viktoriia';
+var myLastName = 'SV';
+
+//7
+var a;
+a = 5;
+var b;
+b = 10;
+var c;
+c = 'I am a'
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//8
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//9
+
+
